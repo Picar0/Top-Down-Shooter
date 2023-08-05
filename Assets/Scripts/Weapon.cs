38a72efb83063886904dae681c325d6971af1574
MUK-Dev <manian1221@gmail.com>
2023-08-05 17:09:19 +0500

    Added firing bullets functionality and rate of fire

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,9 +9,13 @@ public class Weapon : MonoBehaviour
     public static Weapon instance;
 
     //handler
-    [SerializeField] private enum GunType {Semi , Burst , Auto}
+    [SerializeField] private enum GunType { Semi, Burst, Auto }
     [SerializeField] private Transform _spawn;
     [SerializeField] private GunType gunType;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float autoShootTimerMax = .1f;
+
+    private float autoShootTimer = 0f;
 
     private void Awake()
     {
@@ -24,19 +28,29 @@ public class Weapon : MonoBehaviour
         RaycastHit hitInfo;
         float shotDistance = 20;
 
-        if(Physics.Raycast(ray, out hitInfo, shotDistance))
+        if (Physics.Raycast(ray, out hitInfo, shotDistance))
         {
             shotDistance = hitInfo.distance;
         }
-        Debug.DrawRay(ray.origin, ray.direction * shotDistance, Color.red,1);
-        
+        // Debug.DrawRay(ray.origin, ray.direction * shotDistance, Color.red,1);
+        //* Spawn the bullet here with spawn points position and rotation
+        Instantiate(bullet, _spawn.position, _spawn.rotation);
+
+
     }
 
     public void ShootContinuous()
     {
-        if(gunType == GunType.Auto)
+        if (gunType == GunType.Auto)
         {
-            shoot();
+            //? Increase the timer that is needed to shoot
+            autoShootTimer += Time.deltaTime;
+            if (autoShootTimer >= autoShootTimerMax)
+            {
+                //? Here timer was equal to the rate of fire
+                autoShootTimer = 0f;
+                shoot();
+            }
         }
     }
 }
